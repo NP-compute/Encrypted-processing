70d5d01163ef2add15838d58305f5032d89369ae
Luke <anger.luke2004Agmail.com>
2025-09-19 15:50:33 -0400

    Feat: Added in multiple NAND gates construction
    Also added in multiple NAND gate tests
    
    Still need to be able to take the output from one NAND gate and use in another NAND gate

--- src/logic_gate.py
+++ src/logic_gate.py
@@ -3,39 +3,76 @@ def get_bit(value: int, bit_position: int) -> int:
     """Get the bit at the specified position."""
     return (value >> bit_position) & 1
 
-def perform_not_example(value: int) -> int:
-    # This is an example function to demonstrate a not operation
+# NOTE: This is for compilation, figure out a better way to do this
+class pointer:
+    pass
 
-    # Make sure the value is 0 or 1
-    assert value in (0, 1), "Value must be 0 or 1"
+class data:
+    # records the data
+    def __init__(self):
+        self.value: int = 1
+        self.contaminated_pointer: int = 1 # This points to the highest bit that is contaminated
 
-    # Get the first number we have to multiply to negate
-    left = 3
+    def set_bit(self, bit_position: int, bit_value: int):
+        if bit_value not in (0, 1):
+            raise ValueError("bit_value must be 0 or 1")
+        if bit_value == 1:
+            self.value |= (1 << bit_position)
+        else:
+            self.value &= ~(1 << bit_position)
 
-    # Get the second number we have to multiply to negate
-    right = (value << 1) + 1
+    def generate_pointer(self) -> pointer:
+        # Generate a pointer to the next uncontaminated bit
+        new_pointer = pointer(self.contaminated_pointer + 1, self)
+        self.contaminated_pointer += 1
+        return new_pointer
+    
+    def get_len_contaminated(self) -> int:
+        return self.contaminated_pointer + 1
 
-    # Perform the operation
-    answer = left * right
+class pointer:
+    # records the position of the pointer
+    def __init__(self, position: int, data_pointer: data):
+        self.position = position
+        self.data_pointer = data_pointer
 
-    # Extract the bit we want
-    return get_bit(answer, 1)
+    def set_value(self, value: int):
+        self.data_pointer.set_bit(self.position, value)
 
-def perform_nand_example(value1: int, value2: int) -> int:
-    # This is an example function to demonstrate an and operation
+    def get_value(self) -> int:
+        return get_bit(self.data_pointer.value, self.position)
 
-    # Make sure the values are 0 or 1
-    assert value1 in (0, 1), "Value1 must be 0 or 1"
-    assert value2 in (0, 1), "Value2 must be 0 or 1"
+def NAND(pointer_b: pointer, data_a: data, output: data) -> tuple[pointer, pointer]:
+    """ Performs the nand operation
 
-    # Get the first number we have to multiply to negate
-    left = (value1 << 1) + 1
+    Args:
+        pointer_b (pointer): _description_
+        data_a (data): _description_
+        output (data): _description_
 
-    # Get the second number we have to multiply to negate
-    right = (1 << 1) + value2
+    Returns:
+        tuple[pointer, pointer]: (pointer the user can set in a, pointer to the result in output data object)
+    """
+    # Perform the NAND operation
 
-    # Perform the operation
-    answer = left * right
+    data_b: data = pointer_b.data_pointer
 
-    # Extract the bit we want
-    return get_bit(answer, 1)
+    # NOTE: To perform operations with the same data object there will need to be a unitary operation to load the data into another data object
+    assert pointer_b.data_pointer != data_a, "Pointers must point to different data objects"
+
+    # Update the contaminated pointers in the data
+    new_contaminated_pointer = pointer_b.data_pointer.get_len_contaminated() + 2 * data_a.get_len_contaminated()
+    pointer_b.data_pointer.contaminated_pointer = new_contaminated_pointer
+    data_a  .contaminated_pointer = new_contaminated_pointer
+
+    # Make the new pointer a that is to be set
+    pointer_a: pointer = data_a.generate_pointer()
+
+    # Set the value for the 1 for the computation of the NAND
+    output_int: int = pointer_a.position+pointer_b.position
+    data_b.set_bit(output_int, 1)
+
+    # Make the output pointer
+    output_pointer: pointer = pointer(output_int, output)
+
+    return (pointer_a, output_pointer)
--- tst/test_logic_gate.py
+++ tst/test_logic_gate.py
@@ -1,16 +1,94 @@
-from src.logic_gate import perform_not_example, perform_nand_example
+from src.logic_gate import data, pointer, NAND
 
-def test_perform_not_example():
-    assert perform_not_example(0) == 1
-    assert perform_not_example(1) == 0
+def test_NAND(x: int, y: int, expected: int):
+    data_a = data()
+    data_b = data()
+    output = data()
 
-def test_perform_nand_example():
-    assert perform_nand_example(0, 0) == 1
-    assert perform_nand_example(0, 1) == 1
-    assert perform_nand_example(1, 0) == 1
-    assert perform_nand_example(1, 1) == 0
+    # Make a pointer we can set
+    pointer_b = data_b.generate_pointer()
+
+    # Build the NAND gate
+    pointer_a, output_pointer = NAND(pointer_b, data_a, output)
+
+    # Set the values
+    pointer_a.set_value(x)
+    pointer_b.set_value(y)
+
+    # Perform the multiplication process
+    output.value = data_a.value * data_b.value
+
+    assert output_pointer.get_value() == expected, f'Expected {expected} but got {output_pointer.get_value()}\n for data values {data_a.value=}, {data_b.value=}'
+
+def test_NAND_all():
+    test_NAND(0, 0, 1)
+    test_NAND(0, 1, 1)
+    test_NAND(1, 0, 1)
+    test_NAND(1, 1, 0)
+
+def test_two_NANDS():
+    data_a = data()
+    data_b = data()
+    output = data()
+
+    # Make a pointer we can set
+    pointer_b1 = data_b.generate_pointer()
+    pointer_b2 = data_b.generate_pointer()
+
+    # Build the NAND gate
+    pointer_a1, output_pointer1 = NAND(pointer_b1, data_a, output)
+    pointer_a2, output_pointer2 = NAND(pointer_b2, data_a, output)
+
+    # Set the values for the first NAND
+    pointer_a1.set_value(1)
+    pointer_b1.set_value(1)
+
+    # Set the values for the second NAND
+    pointer_a2.set_value(0)
+    pointer_b2.set_value(1)
+
+    # Perform the multiplication process
+    output.value = data_a.value * data_b.value
+
+    assert output_pointer1.get_value() == 0, f'Expected 0 but got {output_pointer1.get_value()}'
+    assert output_pointer2.get_value() == 1, f'Expected 1 but got {output_pointer2.get_value()}'
+
+def test_three_NANDS():
+    data_a = data()
+    data_b = data()
+    output = data()
+
+    # Make a pointer we can set
+    pointer_b1 = data_b.generate_pointer()
+    pointer_b2 = data_b.generate_pointer()
+    pointer_b3 = data_b.generate_pointer()
+
+    # Build the NAND gate
+    pointer_a1, output_pointer1 = NAND(pointer_b1, data_a, output)
+    pointer_a2, output_pointer2 = NAND(pointer_b2, data_a, output)
+    pointer_a3, output_pointer3 = NAND(pointer_b3, data_a, output)
+
+    # Set the values for the first NAND
+    pointer_a1.set_value(1)
+    pointer_b1.set_value(1)
+
+    # Set the values for the second NAND
+    pointer_a2.set_value(0)
+    pointer_b2.set_value(1)
+
+    # Set the values for the third NAND
+    pointer_a3.set_value(1)
+    pointer_b3.set_value(0)
+
+    # Perform the multiplication process
+    output.value = data_a.value * data_b.value
+
+    assert output_pointer1.get_value() == 0, f'Expected 0 but got {output_pointer1.get_value()}'
+    assert output_pointer2.get_value() == 1, f'Expected 1 but got {output_pointer2.get_value()}'
+    assert output_pointer3.get_value() == 1, f'Expected 1 but got {output_pointer3.get_value()}'
 
 if __name__ == "__main__":
-    test_perform_not_example()
-    test_perform_nand_example()
-    print("All tests passed.")
+    test_NAND_all()
+    test_two_NANDS()
+    test_three_NANDS()
+    print("All tests passed for logic_gate.py")
